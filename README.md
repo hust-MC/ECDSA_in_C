e05922b64f9b60178443a39a4584f5f314c4f729
hust-MC <251887584@qq.com>
2019-04-12 14:39:42 +0800

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+#Lite ECDSA in C/C++
---
+++ TestDsa/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  TestDsa
+//
+//  Created by baidu on 2019/4/12.
+//  Copyright © 2019年 baidu. All rights reserved.
+//
+
+#include <stdio.h>
+
+/// dfasfsdf
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    int a = 23;
+    printf("a is : %d " , a);
+    return 0;
+}
